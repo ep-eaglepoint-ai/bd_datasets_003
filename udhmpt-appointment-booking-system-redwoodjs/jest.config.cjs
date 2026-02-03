b1c00742a17b7339974c6c0ba3e3cafe4078767d
Eyuel Nigussie <eyuelnigussie@gmail.com>
2026-02-03 22:32:18 +0300

    fix: jest not foun on docker build error

--- udhmpt-appointment-booking-system-redwoodjs/jest.config.cjs
+++ udhmpt-appointment-booking-system-redwoodjs/jest.config.cjs
@@ -1,8 +1,10 @@
 module.exports = {
-  preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ["**/tests/**/*.test.ts", "**/tests/**/*.spec.ts", "**/tests/**/*.test.tsx"],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
   moduleNameMapper: {
     '^react$': '<rootDir>/node_modules/react',
     '^react-dom$': '<rootDir>/node_modules/react-dom',
