8b99fe16315bcf4f3307161cbb6ddae8f2686a2f
gt-yordanos <gt.yordanos@gmail.com>
2026-02-05 13:02:16 +0300

    fix: modularization error

--- ezcrjn-customer-activity-feature-module-for-e-commerce-platform/repository_after/customer_activity_features.py
+++ ezcrjn-customer-activity-feature-module-for-e-commerce-platform/repository_after/customer_activity_features.py
@@ -11,10 +11,8 @@ The implementation is organized in the internal `customer_activity` package.
 """
 
 from customer_activity.core import CustomerActivityFeatures
-from customer_activity.convenience import (
-    calculate_purchase_features,
-    calculate_session_features,
-)
+from customer_activity.purchase_features import calculate_purchase_features
+from customer_activity.session_features import calculate_session_features
 
 __all__ = [
     "CustomerActivityFeatures",
