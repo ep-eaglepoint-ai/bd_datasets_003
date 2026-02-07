85ca35e41f4b15776fe51afc697b8f3e7111a5c7
nathnael <nathnaelzelalem@gmail.com>
2026-02-07 21:43:18 +0300

    fix the issues

--- a1mxo1-zero-lookahead-markdown-streamer/repository_after/parser.go
+++ a1mxo1-zero-lookahead-markdown-streamer/repository_after/parser.go
@@ -18,6 +18,11 @@ type StreamParser struct {
 	inList bool
 }
 
+type RuneReader interface {
+	ReadRune() (rune, int, error)
+	UnreadRune() error
+}
+
 func (p *StreamParser) Parse(r io.Reader, w io.Writer) error {
 	br := bufio.NewReader(r)
 	bw := bufio.NewWriter(w)
@@ -83,7 +88,7 @@ func (p *StreamParser) handleHeader(first rune, br *bufio.Reader, bw *bufio.Writ
 			return
 		}
 	}
-	
+
 	next, _, _ := br.ReadRune()
 	if next == ' ' {
 		bw.WriteString("<h6>")
@@ -111,32 +116,131 @@ func (p *StreamParser) handleList(first rune, br *bufio.Reader, bw *bufio.Writer
 	}
 }
 
-func (p *StreamParser) handleInline(rn rune, br *bufio.Reader, bw *bufio.Writer) {
-	if rn == '*' {
-		next, _, err := br.ReadRune()
-		if err != nil {
-			bw.WriteRune(rn)
-			return
-		}
-
-		if next == '*' {
-			third, _, err := br.ReadRune()
-			if err == nil && third == '*' {
-				bw.WriteString("<b><i>")
-				p.consumeInline(br, bw, "</i></b>", 3, true)
-			} else {
-				if err == nil { br.UnreadRune() }
-				bw.WriteString("<b>")
-				p.consumeInline(br, bw, "</b>", 2, true)
-			}
+func (p *StreamParser) handleInline(rn rune, br RuneReader, bw *bufio.Writer) {
+	switch rn {
+	case '*':
+		p.handleEmphasis(rn, br, bw)
+	case '[':
+		// Only real stream supports links
+		if realBR, ok := br.(*bufio.Reader); ok {
+			p.handleLink(realBR, bw)
 		} else {
-			br.UnreadRune()
-			bw.WriteString("<i>")
-			p.consumeInline(br, bw, "</i>", 1, false) 
+			bw.WriteRune('[')
 		}
-	} else {
+	default:
 		bw.WriteRune(rn)
 	}
+}
+
+func (p *StreamParser) handleEmphasis(rn rune, br RuneReader, bw *bufio.Writer) {
+	next, _, err := br.ReadRune()
+	if err != nil {
+		bw.WriteRune(rn)
+		return
+	}
+
+	if next == '*' {
+		third, _, err := br.ReadRune()
+		if err == nil && third == '*' {
+			bw.WriteString("<b><i>")
+			p.consumeInline(br, bw, "</i></b>", 3, true)
+		} else {
+			if err == nil {
+				br.UnreadRune()
+			}
+			bw.WriteString("<b>")
+			p.consumeInline(br, bw, "</b>", 2, true)
+		}
+	} else {
+		br.UnreadRune()
+		bw.WriteString("<i>")
+		p.consumeInline(br, bw, "</i>", 1, false)
+	}
+}
+
+func (p *StreamParser) handleLink(br *bufio.Reader, bw *bufio.Writer) {
+	var textBuf []rune
+	for {
+		rn, _, err := br.ReadRune()
+		if err != nil || rn == '\n' {
+			bw.WriteRune('[')
+			bw.WriteString(string(textBuf))
+			if rn == '\n' {
+				br.UnreadRune()
+			}
+			return
+		}
+		if rn == ']' {
+			break
+		}
+		textBuf = append(textBuf, rn)
+	}
+
+	next, _, _ := br.ReadRune()
+	if next != '(' {
+		bw.WriteRune('[')
+		bw.WriteString(string(textBuf))
+		bw.WriteRune(']')
+		br.UnreadRune()
+		return
+	}
+
+	var urlBuf []rune
+	for {
+		rn, _, err := br.ReadRune()
+		if err != nil || rn == '\n' {
+			bw.WriteString("[")
+			bw.WriteString(string(textBuf))
+			bw.WriteString("](")
+			bw.WriteString(string(urlBuf))
+			if rn == '\n' {
+				br.UnreadRune()
+			}
+			return
+		}
+		if rn == ')' {
+			break
+		}
+		urlBuf = append(urlBuf, rn)
+	}
+
+	fmt.Fprintf(bw, "<a href=\"%s\">", string(urlBuf))
+
+	innerReader := javaStringReader(string(textBuf))
+	for {
+		r, _, err := innerReader.ReadRune()
+		if err == io.EOF {
+			break
+		}
+		p.handleInline(r, innerReader, bw)
+	}
+
+	bw.WriteString("</a>")
+}
+
+type stringReader struct {
+	str []rune
+	pos int
+}
+
+func (r *stringReader) Read(p []byte) (n int, err error) { return 0, io.EOF }
+func (r *stringReader) ReadRune() (rune, int, error) {
+	if r.pos >= len(r.str) {
+		return 0, 0, io.EOF
+	}
+	rn := r.str[r.pos]
+	r.pos++
+	return rn, 0, nil
+}
+func (r *stringReader) UnreadRune() error {
+	if r.pos <= 0 {
+		return fmt.Errorf("cannot unread")
+	}
+	r.pos--
+	return nil
+}
+func javaStringReader(s string) *stringReader {
+	return &stringReader{str: []rune(s)}
 }
 
 func (p *StreamParser) consumeUntilNewline(br *bufio.Reader, bw *bufio.Writer, tagClose string) {
@@ -156,14 +260,22 @@ func (p *StreamParser) consumeUntilNewline(br *bufio.Reader, bw *bufio.Writer, t
 	}
 }
 
-func (p *StreamParser) consumeInline(br *bufio.Reader, bw *bufio.Writer, tagClose string, markerCount int, forceClose bool) {
+func (p *StreamParser) consumeInline(
+	br RuneReader,
+	bw *bufio.Writer,
+	tagClose string,
+	markerCount int,
+	forceClose bool,
+) {
 	for {
 		rn, _, err := br.ReadRune()
 		if err == io.EOF || rn == '\n' {
 			if forceClose {
 				bw.WriteString(tagClose)
 			}
-			if rn == '\n' { br.UnreadRune() }
+			if rn == '\n' {
+				br.UnreadRune()
+			}
 			return
 		}
 
@@ -182,7 +294,9 @@ func (p *StreamParser) consumeInline(br *bufio.Reader, bw *bufio.Writer, tagClos
 				bw.WriteString(tagClose)
 				return
 			}
-			for i := 0; i < count; i++ { bw.WriteRune('*') }
+			for i := 0; i < count; i++ {
+				bw.WriteRune('*')
+			}
 		} else {
 			bw.WriteRune(rn)
 		}
