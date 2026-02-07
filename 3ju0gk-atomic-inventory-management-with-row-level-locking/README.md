aa37b86f272a1392b0ac95ce685e287557818998
YohannesGetahunSimegn <ygetahun37@gmail.com>
2026-02-07 15:11:55 +0300

    bug fix on entry

--- 3ju0gk-atomic-inventory-management-with-row-level-locking/README.md
+++ 3ju0gk-atomic-inventory-management-with-row-level-locking/README.md
@@ -4,7 +4,7 @@
     No command for before
 
     ## After Test Docker Command
-    docker compose exec app sh -c "export PYTHONPATH=\$PYTHONPATH:/app:/app/repository_after && python manage.py test tests --noinput --keepdb"
+    docker compose exec app sh -c 'export PYTHONPATH=\$PYTHONPATH:/app:/app/repository_after && python manage.py test tests --noinput --keepdb'
 
     ## Evaluation Docker Command
     docker compose exec app python /app/evaluation/evaluation.py
--- 3ju0gk-atomic-inventory-management-with-row-level-locking/repository_after/entrypoint.sh
+++ 3ju0gk-atomic-inventory-management-with-row-level-locking/repository_after/entrypoint.sh
@@ -13,9 +13,9 @@ from decimal import Decimal
 
 if not Product.objects.exists():
     print('Seeding database...')
-    Product.objects.get_or_create(id=1, defaults={'name': 'Gaming Laptop', 'stock': 5, 'price': Decimal('999.00'), 'active': True})
+    Product.objects.get_or_create(id=1, defaults={'name': 'Gaming Laptop', 'stock': 5, 'price': Decimal('9.00'), 'active': True})
     Product.objects.get_or_create(id=2, defaults={'name': 'Mechanical Keyboard', 'stock': 20, 'price': Decimal('149.00'), 'active': True})
-    Product.objects.get_or_create(id=3, defaults={'name': 'Wireless Mouse', 'stock': 15, 'price': Decimal('79.00'), 'active': True})
+    Product.objects.get_or_create(id=3, defaults={'name': 'Wireless Mouse', 'stock': 15, 'price': Decimal('7.00'), 'active': True})
     
     Wallet.objects.get_or_create(user_id=1, defaults={'balance': Decimal('2000.00')})
     Wallet.objects.get_or_create(user_id=2, defaults={'balance': Decimal('50.00')})
@@ -26,3 +26,5 @@ else:
 
 
 exec "$@"
+
+this entry is putting price above fund 
