041bb5fb2ae2cc8e14a82fe08d88d2bc72d29d04
mariatesfaye <mariatesfaye67@gmail.com>
2026-02-08 15:39:24 +0300

    fix comment

--- kef0ja-sticky-notes-reorder-categorize/repository_after/jest.config.js
+++ kef0ja-sticky-notes-reorder-categorize/repository_after/jest.config.js
@@ -1,10 +1,35 @@
 module.exports = {
-  roots: ['<rootDir>/src', '<rootDir>/../../tests'],
+  testEnvironment: 'jsdom',
+
+  roots: ['<rootDir>/src', '<rootDir>/../tests'],
   testMatch: [
     '<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}',
     '<rootDir>/src/**/*.{spec,test}.{js,jsx,ts,tsx}',
-    '<rootDir>/../../tests/**/*.{spec,test}.{js,jsx,ts,tsx}'
+    '<rootDir>/../tests/**/*.{spec,test}.{js,jsx,ts,tsx}'
   ],
   testPathIgnorePatterns: ['/node_modules/'],
-  setupFilesAfterEnv: ['<rootDir>/src/setupTests.js']
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
+
+  haste: {
+    throwOnModuleCollision: false,
+  },
+
+  fakeTimers: {
+    enableGlobally: true,
+  },
+
+  transform: {
+    '^.+\\.(js|jsx)$': 'babel-jest',
+    '^.+\\.css$': 'jest-transform-stub',
+  },
+
+  moduleNameMapper: {
+    '^react$': '<rootDir>/node_modules/react',
+    '^react-dom$': '<rootDir>/node_modules/react-dom',
+    '^react-dom/client$': '<rootDir>/node_modules/react-dom/client',
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': 'jest-transform-stub',
+  },
+
+  moduleDirectories: ['node_modules'],
 };
